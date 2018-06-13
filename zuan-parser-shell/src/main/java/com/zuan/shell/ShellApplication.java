7b8a31600e2fd8c90e508d2ecdfd390bfa6c3e00
Quan Minh Doan <minhquan9402@gmail.com>
2018-06-13 22:07:59 +0700

    Update sh file gateway server

--- zuan-parser-shell/src/main/java/com/zuan/shell/ShellApplication.java
+++ zuan-parser-shell/src/main/java/com/zuan/shell/ShellApplication.java
@@ -7,10 +7,6 @@ import java.io.IOException;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.domain.EntityScan;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
-
-import com.zuan.data.repositories.BaseJpaRepositoryImpl;
 
 /**
  * @author <a href="mailto:developer@hitachiconsulting.com">quanmd.nv</a>
@@ -26,7 +22,7 @@ public class ShellApplication {
    * @throws IOException
    */
   public static void main(String[] args) throws IOException {
-    SpringApplication application = new SpringApplication(ShellApplication.class);
+    final SpringApplication application = new SpringApplication(ShellApplication.class);
     application.run(args);
   }
 }
